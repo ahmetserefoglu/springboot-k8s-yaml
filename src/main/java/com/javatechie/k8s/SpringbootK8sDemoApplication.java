6f12a8226b4f93a6c80454400e6ff54d6ce4679a
Ahmet Şerefoğlu <ahmet.serefoglu@pureenergy.com.tr>
2024-10-13 10:09:19 +0300

    Change Git

--- src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -1,21 +1,34 @@
 package com.javatechie.k8s;
 
+import com.javatechie.k8s.job.ReportGenerationScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class SpringbootK8sDemoApplication {
+//@RestController
+@EnableScheduling
+public class SpringbootK8sDemoApplication implements CommandLineRunner {
 
-	@GetMapping("/message")
+    @Autowired
+    private ReportGenerationScheduler scheduler;
+
+	/*@GetMapping("/message")
 	public String displayMessage(){
 		return "Congratulation you successfully deployed your application to kubernetes !!";
-	}
+	}*/
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootK8sDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootK8sDemoApplication.class, args);
+    }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        scheduler.generateReportAndSendEmail();
+    }
 }
